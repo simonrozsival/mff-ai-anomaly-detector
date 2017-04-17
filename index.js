e0aa1b410482c1936f6b91bf9815d18bc7f42de7
simonrozsival <Simon Rozsival>
2017-04-17 14:54:59 +0200

    adds smoothing to the main loop

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import readline from 'readline';
 
-import { parseSensorReading, shiftWindow, detectAnomaly } from './src';
+import { parseSensorReading, shiftWindow, isAnomalous, smooth } from './src';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,13 +9,16 @@ const rl = readline.createInterface({
 });
 
 let window = null; // sliding window - the history of the nominal points (sensoric readings)
+let prev = null;
 
 rl.on('line', line => {
   const data = parseSensorReading(line);
 
-  if (detectAnomaly(data, window)) {
+  if (isAnomalous(smooth(prev, data), window)) {
     rl.write(`Anomaly detected: ${line}`);
   } else {
     window = shiftWindow(data); // the window might contain only nominal data
   }
+
+  prev = data;
 });
