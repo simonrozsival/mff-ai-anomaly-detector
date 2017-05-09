79a262692d37a5256f8533909eca98cde155352d
Šimon Rozsíval <simon@rozsival.com>
2017-05-09 10:25:28 +0200

    cleanup

--- src/mahalanobis.py
+++
@@ -1,8 +0,0 @@
-from numpy import inv, cov, mean
-from scipy.spatial.distance import mahalanobis
-
-def distance(x, data):
-    means = mean(data, axis=1)
-    icov = inv(cov(data))
-    return mahalanobis(x, means, icov)
-
